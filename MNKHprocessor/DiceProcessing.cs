fb8d091a722e9bee0301e952eb0487ce2e6004b1
Steven Knipe <knipesteven@gmail.com>
2025-05-15 11:27:24 -0700

    T93 Results

--- MNKHprocessor/DiceProcessing.cs
+++ MNKHprocessor/DiceProcessing.cs
@@ -56,6 +56,7 @@ namespace MNKHprocessor
                             }
                             continue;
                         }
+                        if (s.Contains("Cancel Project")) { continue; }
                         planList.Add(s);
                     }
                 }
